d6bd387307481702257a166965a760a0c2baaeb6
Omega <73088258+PlayInfinityy@users.noreply.github.com>
2024-07-23 07:52:51 -0400

    Update app.js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -8,7 +8,7 @@ class _app {
   backgroundToggler = false;
   shouldIgnoreVideo = false;
   effects = ['bounce', 'flash', 'pulse', 'rubberBand', 'shake', 'swing', 'tada', 'wobble', 'jello'];
-  brandDescription = ['11 years old kids team', 'pro roblox players', 'xane ddosers', 'trashtalkers', 'omegatm fanboys', 'hack vs hack team'];
+  brandDescription = ['pro ragers', 'lobby slammers', 'xane ddosers', 'trashtalkers', 'omegatm fanboys', 'powered by velocity', 'hack vs hack team'];
 
   titleChanger = (text, delay) => {
     if (!text) return;
