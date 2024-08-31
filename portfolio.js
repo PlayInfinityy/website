f9e57cd31e4ff132fe6a0452c968a6c16d63fce8
Omega <73088258+PlayInfinityy@users.noreply.github.com>
2024-08-30 22:19:54 -0400

    Add files via upload

--- portfolio.js
+++ portfolio.js
@@ -7,6 +7,8 @@ const timeouts = [];
 const mobileAndTabletCheck = () => /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
 
 $(document).ready(() => {
+    console.log('Document is ready');
+
     const links = [
         {
             "name": "Omega",
@@ -120,178 +122,188 @@ $(document).ready(() => {
 
     app.titleChanger(['Best', 'Ragers', 'In', 'North', 'America', '=', 'omegatm.wtf', 'Song name: Day & Night']);
     app.iconChanger(['assets/icons/roses/rose1.jpg', 'assets/icons/roses/rose2.jpg', 'assets/icons/roses/rose3.jpg', 'assets/icons/roses/rose4.jpg', 'assets/icons/roses/rose5.jpg', 'assets/icons/roses/rose6.jpg', 'assets/icons/roses/rose7.jpg', 'assets/icons/roses/rose8.jpg', 'assets/icons/roses/rose1.jpg']);
-});
 
-if ($.cookie('videoTime')) {
-    app.videoElement.currentTime = $.cookie('videoTime');
-    app.audioElement.currentTime = $.cookie('videoTime');
-}
+    const path = window.location.pathname;
+    console.log('Current path:', path);
+    if (path === '/' || path === '/index.html' || path.endsWith('/index.html')) {
+        console.log('On the main page');
 
-document.addEventListener('contextmenu', (event) => {
-    event.preventDefault();
-});
-
-document.body.onkeyup = (event) => {
-    if (event.keyCode == 32 && app.skippedIntro) {
-        if (app.backgroundToggler) {
-            app.videoElement.play();
-            app.audioElement.play();
-        } else {
-            app.videoElement.pause();
-            app.audioElement.pause();
+        if ($.cookie('videoTime')) {
+            app.videoElement.currentTime = $.cookie('videoTime');
+            app.audioElement.currentTime = $.cookie('videoTime');
         }
 
-        return (app.backgroundToggler = !app.backgroundToggler);
-    }
-};
-
-$('html').on('contextmenu', (event) => {
-    const img = document.createElement('img');
-
-    const trollfaceLight = app.skippedIntro ? '' : 'trollface-light';
-
-    img.src = 'assets/others/trollface.jpg';
-    img.width = 64;
-    img.height = 64;
-    img.alt = 'omegatm.wtf';
-    img.style = `position: absolute; left: ${event.pageX}px; top: ${event.pageY}px; z-index: 10`;
-    img.className = `troll ${trollfaceLight}`;
-
-    document.body.appendChild(img);
-});
-
-setInterval(() => {
-    $('.troll').remove();
-}, 600);
-
-$('.skip').click(() => {
-    skipIntro();
-});
-
-$.fn.extend({
-    animateCss: function (animationName) {
-        const animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
-
-        this.addClass(`animated ${animationName}`).one(animationEnd, () => {
-            $(this).removeClass(`animated ${animationName}`);
+        document.addEventListener('contextmenu', (event) => {
+            event.preventDefault();
         });
 
-        return this;
-    },
-});
+        document.body.onkeyup = (event) => {
+            if (event.keyCode == 32 && app.skippedIntro) {
+                if (app.backgroundToggler) {
+                    app.videoElement.play();
+                    app.audioElement.play();
+                } else {
+                    app.videoElement.pause();
+                    app.audioElement.pause();
+                }
 
-const writeLine = (text, speed, timeout, callback) => {
-    timeout = typeof timeout === 'number' ? timeout : [0, (callback = timeout)];
+                return (app.backgroundToggler = !app.backgroundToggler);
+            }
+        };
 
-    const lineNumber = app.id !== 2 ? ++app.id : (app.id += 2);
+        $('html').on('contextmenu', (event) => {
+            const img = document.createElement('img');
 
-    setTimeout(() => {
-        const typed = new Typed(`#line${lineNumber}`, {
-            strings: text,
-            typeSpeed: speed,
-            onComplete: callback,
+            const trollfaceLight = app.skippedIntro ? '' : 'trollface-light';
+
+            img.src = 'assets/others/trollface.jpg';
+            img.width = 64;
+            img.height = 64;
+            img.alt = 'omegatm.wtf';
+            img.style = `position: absolute; left: ${event.pageX}px; top: ${event.pageY}px; z-index: 10`;
+            img.className = `troll ${trollfaceLight}`;
+
+            document.body.appendChild(img);
         });
-    }, timeout);
-};
 
-$.getJSON(ipgeolocation, (data) => {
-    writeLine(['Authenticating...', "Granting access to <span style='font-size: 14px; color: #06d;'>[Ragers Hall Of Fame]</span>..."], 30, () => {
-        if (app.skippedIntro) return;
+        setInterval(() => {
+            $('.troll').remove();
+        }, 600);
 
-        clearCursor();
+        $('.skip').click(() => {
+            skipIntro();
+        });
 
-        const usernames = ['user', 'dude'];
+        $.fn.extend({
+            animateCss: function (animationName) {
+                const animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
 
-        const ip = data.ip ? data.ip : usernames[Math.floor(Math.random() * usernames.length)];
-        const country = data.country_name ? data.country_name : 'your country';
+                this.addClass(`animated ${animationName}`).one(animationEnd, () => {
+                    $(this).removeClass(`animated ${animationName}`);
+                });
 
-        writeLine([`Access granted! <span style='font-size: 14px; color: #0f0;'>[success]</span>`, `Welcome back, <i style='color: #0f0'>${ip}</i>! By the way, nice to see someone from ${country} here!`], 30, 500, () => {
+                return this;
+            },
+        });
+
+        const writeLine = (text, speed, timeout, callback) => {
+            timeout = typeof timeout === 'number' ? timeout : [0, (callback = timeout)];
+
+            const lineNumber = app.id !== 2 ? ++app.id : (app.id += 2);
+
+            setTimeout(() => {
+                const typed = new Typed(`#line${lineNumber}`, {
+                    strings: text,
+                    typeSpeed: speed,
+                    onComplete: callback,
+                });
+            }, timeout);
+        };
+
+        $.getJSON(ipgeolocation, (data) => {
+            console.log('Geolocation data received', data);
+
+            writeLine(['Authenticating...', "Granting access to <span style='font-size: 14px; color: #06d;'>[Ragers Hall Of Fame]</span>..."], 30, () => {
+                if (app.skippedIntro) return;
+
+                clearCursor();
+
+                const usernames = ['user', 'dude'];
+
+                const ip = data.ip ? data.ip : usernames[Math.floor(Math.random() * usernames.length)];
+                const country = data.country_name ? data.country_name : 'your country';
+
+                writeLine([`Access granted! <span style='font-size: 14px; color: #0f0;'>[success]</span>`, `Welcome back, <i style='color: #0f0'>${ip}</i>! By the way, nice to see someone from ${country} here!`], 30, 500, () => {
+                    if (app.skippedIntro) return;
+
+                    clearCursor();
+
+                    writeLine([`<i style='color: #F62459'>omegatm.wtf $$</i>`], 120, 500, () => {
+                        timeouts.push(
+                            setTimeout(() => {
+                                if (app.skippedIntro) return;
+
+                                clearCursor();
+
+                                setTimeout(() => {
+                                    skipIntro();
+                                }, 500);
+                            }, 1000)
+                        );
+                    });
+                });
+            });
+        });
+
+        const skipIntro = () => {
             if (app.skippedIntro) return;
 
-            clearCursor();
+            app.skippedIntro = true;
 
-            writeLine([`<i style='color: #F62459'>omegatm.wtf $$</i>`], 120, 500, () => {
-                timeouts.push(
-                    setTimeout(() => {
-                        if (app.skippedIntro) return;
-
-                        clearCursor();
-
-                        setTimeout(() => {
-                            skipIntro();
-                        }, 500);
-                    }, 1000)
-                );
+            timeouts.forEach((timeout) => {
+                clearTimeout(timeout);
             });
-        });
-    });
+
+            $('.top-right').remove();
+
+            $('#main').fadeOut(100, () => {
+                $('#main').remove();
+
+                $('#marquee').marquee({
+                    duration: 15000,
+                    gap: 420,
+                    delayBeforeStart: 1000,
+                    direction: 'left',
+                    duplicated: true,
+                });
+
+                setTimeout(() => {
+                    $('.brand-header').animateCss(app.effects[Math.floor(Math.random() * app.effects.length)]);
+                }, 200);
+
+                setTimeout(() => {
+                    const typed = new Typed('#brand', {
+                        strings: app.brandDescription,
+                        typeSpeed: 40,
+
+                        onComplete: () => {
+                            clearCursor();
+                        },
+                    });
+                }, 1350);
+
+                setTimeout(() => {
+                    if (!app.shouldIgnoreVideo) {
+                        app.videoElement.play();
+                        app.audioElement.play();
+                    }
+
+                    app.videoElement.addEventListener(
+                        'timeupdate',
+                        () => {
+                            $.cookie('videoTime', app.videoElement.currentTime, { expires: 1 });
+                        },
+                        false
+                    );
+
+                    $('.marquee-container').css('visibility', 'visible').hide().fadeIn(100);
+
+                    $('.marquee-container').animateCss('zoomIn');
+
+                    $('.container').fadeIn();
+
+                    $('.background').fadeIn(200, () => {
+                        if (!app.shouldIgnoreVideo) $('#audio').animate({ volume: app.musicVolume }, app.musicFadeIn);
+                        $('.header').fadeIn();
+                    });
+                }, 200);
+            });
+        };
+
+        const clearCursor = () => {
+            return $('span').siblings('.typed-cursor').css('opacity', '0');
+        };
+    } else {
+        console.log('Not on the main page');
+    }
 });
-
-const skipIntro = () => {
-    if (app.skippedIntro) return;
-
-    app.skippedIntro = true;
-
-    timeouts.forEach((timeout) => {
-        clearTimeout(timeout);
-    });
-
-    $('.top-right').remove();
-
-    $('#main').fadeOut(100, () => {
-        $('#main').remove();
-
-        $('#marquee').marquee({
-            duration: 15000,
-            gap: 420,
-            delayBeforeStart: 1000,
-            direction: 'left',
-            duplicated: true,
-        });
-
-        setTimeout(() => {
-            $('.brand-header').animateCss(app.effects[Math.floor(Math.random() * app.effects.length)]);
-        }, 200);
-
-        setTimeout(() => {
-            const typed = new Typed('#brand', {
-                strings: app.brandDescription,
-                typeSpeed: 40,
-
-                onComplete: () => {
-                    clearCursor();
-                },
-            });
-        }, 1350);
-
-        setTimeout(() => {
-            if (!app.shouldIgnoreVideo) {
-                app.videoElement.play();
-                app.audioElement.play();
-            }
-
-            app.videoElement.addEventListener(
-                'timeupdate',
-                () => {
-                    $.cookie('videoTime', app.videoElement.currentTime, { expires: 1 });
-                },
-                false
-            );
-
-            $('.marquee-container').css('visibility', 'visible').hide().fadeIn(100);
-
-            $('.marquee-container').animateCss('zoomIn');
-
-            $('.container').fadeIn();
-
-            $('.background').fadeIn(200, () => {
-                if (!app.shouldIgnoreVideo) $('#audio').animate({ volume: app.musicVolume }, app.musicFadeIn);
-            });
-        }, 200);
-    });
-};
-
-const clearCursor = () => {
-    return $('span').siblings('.typed-cursor').css('opacity', '0');
-};
-
