6678769d993f61b3d4eaf3c260647cdaf6113424
Omega <73088258+PlayInfinityy@users.noreply.github.com>
2024-11-14 03:43:54 -0500

    Add files via upload

---
+++ assets/js/snakesData.js
@@ -0,0 +1,20 @@
+const snakes = [
+    {
+        id: 1,
+        username: "Kimbo1450 / .whoerror",
+        image: "assets/images/snakes/snake1.png",
+        betrayalDate: "2024",
+        snakeLevel: "King of the Snakes",
+        description: "The biggest snake in history—betrayed a friend of over two years for the sake of a woman. A broke, desperate online 'eboy' who can't even scrape together enough for a Rust account or a basic VPN subscription. Relies on others' money like a leech, living in a constant state of cowardice and entitlement."
+    },
+    {
+        id: 2,
+        username: "constantlymia",
+        image: "assets/images/snakes/snake2.png",
+        betrayalDate: "2024",
+        snakeLevel: "Mistress of King Snake",
+        description: "She threw away someone who had their life together, who was ready to give her everything, just to chase after a fucking eboy. Walked away from stability, loyalty, and real love, all for the attention of some immature, attention-seeking loser who offered her nothing but empty promises and drama. She chose chaos over comfort, and now she's stuck with the mess she made."
+    }
+];
+
+export default snakes;
