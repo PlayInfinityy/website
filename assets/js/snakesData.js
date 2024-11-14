89463200ec11945da6011acbc5b39a3963cce86d
Omega <73088258+PlayInfinityy@users.noreply.github.com>
2024-11-14 03:45:42 -0500

    Update snakesData.js

--- assets/js/snakesData.js
+++ assets/js/snakesData.js
@@ -2,15 +2,15 @@ const snakes = [
     {
         id: 1,
         username: "Kimbo1450 / .whoerror",
-        image: "assets/images/snakes/snake1.png",
+        image: "assets/images/snakes/kimbo.png",
         betrayalDate: "2024",
         snakeLevel: "King of the Snakes",
-        description: "The biggest snake in history—betrayed a friend of over two years for the sake of a woman. A broke, desperate online 'eboy' who can't even scrape together enough for a Rust account or a basic VPN subscription. Relies on others' money like a leech, living in a constant state of cowardice and entitlement."
+        description: "The biggest snake in historyâ€”betrayed a friend of over two years for the sake of a woman. A broke, desperate online 'eboy' who can't even scrape together enough for a Rust account or a basic VPN subscription. Relies on others' money like a leech, living in a constant state of cowardice and entitlement."
     },
     {
         id: 2,
         username: "constantlymia",
-        image: "assets/images/snakes/snake2.png",
+        image: "assets/images/snakes/mia.png",
         betrayalDate: "2024",
         snakeLevel: "Mistress of King Snake",
         description: "She threw away someone who had their life together, who was ready to give her everything, just to chase after a fucking eboy. Walked away from stability, loyalty, and real love, all for the attention of some immature, attention-seeking loser who offered her nothing but empty promises and drama. She chose chaos over comfort, and now she's stuck with the mess she made."
