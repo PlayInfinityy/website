a5eea5bbce03eeb90b11b15ff9322eed6f6f9f90
Omega <73088258+PlayInfinityy@users.noreply.github.com>
2024-12-12 21:40:53 -0500

    Update snakesData.js

--- assets/js/snakesData.js
+++ assets/js/snakesData.js
@@ -4,7 +4,6 @@ const snakes = [
         username: "Kimbo1450 / .whoerror",
         image: "assets/images/snakes/kimbo.png",
         betrayalDate: "2024",
-        snakeLevel: "King of the Snakes",
         description: "The biggest snake in history—betrayed a friend of over two years for the sake of a woman. A broke, desperate online 'eboy' who can't even scrape together enough for a Rust account or a basic VPN subscription. Relies on others' money like a leech, living in a constant state of cowardice and entitlement."
     },
     {
@@ -12,8 +11,14 @@ const snakes = [
         username: "constantlymia",
         image: "assets/images/snakes/mia.png",
         betrayalDate: "2024",
-        snakeLevel: "Mistress of King Snake",
         description: "She threw away someone who had their life together, who was ready to give her everything, just to chase after a fucking eboy. Walked away from stability, loyalty, and real love, all for the attention of some immature, attention-seeking loser who offered her nothing but empty promises and drama. She chose chaos over comfort, and now she's stuck with the mess she made."
+    },
+    {
+        id: 3,
+        username: ".whosfrosty/.whoscalico",
+        image: "assets/images/snakes/frosty.png",
+        betrayalDate: "2024",
+        description: "Attempted to dox OmegaTM, https://doxbin.org/upload/whosfrostywhoscalico. In return got full doxed and ran out of com."
     }
 ];
 
